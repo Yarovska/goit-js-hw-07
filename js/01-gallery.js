a33afeef4d499f9552352b2f52658e2d932b496b
Yarovska <121119275+Yarovska@users.noreply.github.com>
2023-05-17 17:56:41 +0300

    new update

---
+++ js/01-gallery.js
@@ -0,0 +1,51 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+
+
+const galleryContainer = document.querySelector(".gallery");
+const result = createGallery(galleryItems);
+
+function createGallery(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", result);
+
+function onGalleryPhotoClick(event) {
+  event.preventDefault();
+
+  const modal = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`,
+    {
+      onShow: () => window.addEventListener("keydown", onPressKeyEscape),
+      onClose: () => window.removeEventListener("keydown", onPressKeyEscape),
+    }
+  );
+  modal.show();
+
+  function onPressKeyEscape(event) {
+    if (event.code === "Escape") {
+      modal.close();
+    }
+  }
+}
+
+galleryContainer.addEventListener("click", onGalleryPhotoClick);
